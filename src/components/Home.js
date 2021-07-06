b1be01798f676d1f544d11f30d826bff95314c4a
gyeolse <threewave@kakao.com>
2021-07-06 23:58:17 +0900

    MOD: Apply Material-UI and replace icon image     Details             - modify Home.js             - apply subway icon/png in project.

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import axios from "axios";
 import { CONFIG_KEY } from "../config/Key";
 import Train from "./Train";
+import Button from "@material-ui/core/Button";
+import { FormControl, Select, InputLabel, MenuItem } from "@material-ui/core";
 
 function Home() {
   const [result, setresult] = useState([]);
@@ -20,7 +22,12 @@ function Home() {
       .get(url)
       .then(function (res) {
         // console.log(res.data.realtimePositionList);
-        setresult(res.data.realtimePositionList);
+        if (result !== null) {
+          setresult([]);
+          setresult(res.data.realtimePositionList);
+        } else {
+          setresult(res.data.realtimePositionList);
+        }
       })
       .catch(function (err) {
         console.log(err);
@@ -40,9 +47,28 @@ function Home() {
     <div className="App">
       <div>
         <h1>호선 입력 후, 결과 확인</h1>
-        <input type="number" name="line" onChange={lineChange} />
+        <FormControl>
+          <InputLabel id="demo-simple-select-label">호선</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={line}
+            onChange={lineChange}
+          >
+            <MenuItem value={1}>1호선</MenuItem>
+            <MenuItem value={2}>2호선</MenuItem>
+            <MenuItem value={3}>3호선</MenuItem>
+            <MenuItem value={4}>4호선</MenuItem>
+            <MenuItem value={5}>5호선</MenuItem>
+            <MenuItem value={6}>6호선</MenuItem>
+            <MenuItem value={7}>7호선</MenuItem>
+            <MenuItem value={8}>8호선</MenuItem>
+            <MenuItem value={9}>9호선</MenuItem>
+          </Select>
+        </FormControl>
+        {/* <input type="number" name="line" onChange={lineChange} /> */}
 
-        <button onClick={buttonClickListener}>Test Button</button>
+        <Button onClick={buttonClickListener}>Test Button</Button>
       </div>
       {/* {result.map((train) => {
           {
